58c06c022c12b8de1e598bd2164deb8555bdaa37
DahmanCode <elaammariabderrahman@gmail.com>
2022-11-09 00:04:03 +0100

    add the Route for every page i added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import {
 import Header from './components/common/header/Header';
 import Home from "./components/home/Home"
 import About from "./components/about/About"
-import Contact from "./components/contact/Contact"
 import Event from "./components/event/Event"
+import News from "./components/news/News"
+import Contact from "./components/contact/Contact"
 import Previous from './components/event/previous/Previous';
+import Upcoming from './components/event/upcoming/Upcoming';
 
 function App() {
   return (
@@ -19,9 +21,11 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
-          <Route exact path='/contact' component={Contact} />
           <Route exact path='/event' component={Event} />
+          <Route exact path='/news' component={News} />
+          <Route exact path='/contact' component={Contact} />
           <Route exact path='/previous' component={Previous} />
+          <Route exact path='/upcoming' component={Upcoming} />
         </Switch>
       </Router>
     </>
